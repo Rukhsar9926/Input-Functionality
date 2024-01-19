9f551bdd58ffd8e6eed2cb46b29064cdf73d74db
Rukhsar9926 <153531256+Rukhsar9926@users.noreply.github.com>
2024-01-19 16:40:15 +0530

    Done some Modifications

---
+++ src/Components/Form.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Form = () => {
+    
+
+    const [fname, setfname] = useState("")
+    
+    const [display, setDisplay] = useState("");
+
+    const handleChange = e => {
+        setfname(e.target.value)
+    };
+
+
+
+    const addList = (e) => {
+        setDisplay(fname);
+        setfname("")
+
+
+    };
+
+
+
+
+
+
+
+    return (
+        <>
+
+
+
+
+            <form className='mt-5' >
+                <div className='d-flex '>
+                    <div>
+
+                        <input type="text" class="form-control" placeholder="Enter" value={fname} onChange={handleChange}/>
+
+                    </div>
+                    <div>
+                        <button class="btn btn-primary" type="button" onClick={addList} >Add</button>
+                    </div>
+                </div>
+
+                <div className='mt-3'>
+                    <ul class="list-group" >
+                        <li class="list-group-item"> Enter your name :{display}
+                        <button type="button" class="btn btn-danger float-end">Delete</button>
+                        <button type="button" class="btn btn-warning  float-end">Edit</button>
+                          </li>
+                        
+                    </ul>
+                </div>
+
+
+            </form>
+
+
+
+        </>
+    )
+}
+
+export default Form
